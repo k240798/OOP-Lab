35bbf3dcf92451149d54a57cc9d7559e9a055133
k240798 <k240798@nu.edu.pk>
2025-02-14 10:02:43 +0500

    Update q5.cpp

--- Lab Task 3/q5.cpp
+++ Lab Task 3/q5.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Book {
 private:
     string bookName;
-    string isbnNumber;
+    string ISBN;
     string authorName;
     string publisher;
 
 public:
-    Book(string bookName, string isbnNumber, string authorName, string publisher) {
-        this->bookName = bookName;
-        this->isbnNumber = isbnNumber;
-        this->authorName = authorName;
-        this->publisher = publisher;
+    // Constructor that initializes the Book instance with provided data
+    Book(string bName, string isbn, string aName, string pub) {
+        this->bookName = bName;
+        this->ISBN = isbn;
+        this->authorName = aName;
+        this->publisher = pub;
     }
 
+    // Query Methods for each attribute
     string getBookName() const {
         return bookName;
     }
 
-    string getIsbnNumber() const {
-        return isbnNumber;
+    string getISBN() const {
+        return ISBN;
     }
 
     string getAuthorName() const {
@@ -33,23 +36,39 @@ public:
         return publisher;
     }
 
+    // Method to return the book's description as a string
     string getBookInfo() const {
-        return "Book Name: " + bookName + "\nISBN Number: " + isbnNumber + "\nAuthor: " + authorName + "\nPublisher: " + publisher;
+        return "Book Name: " + bookName + "\n" +
+               "ISBN: " + ISBN + "\n" +
+               "Author: " + authorName + "\n" +
+               "Publisher: " + publisher;
     }
 };
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Ensure the program is provided with exactly 20 arguments (5 books * 4 details per book)
+    if (argc != 21) {
+        cout << "Usage: BookTest <BookName1> <ISBN1> <Author1> <Publisher1> "
+             << "<BookName2> <ISBN2> <Author2> <Publisher2> "
+             << "<BookName3> <ISBN3> <Author3> <Publisher3> "
+             << "<BookName4> <ISBN4> <Author4> <Publisher4> "
+             << "<BookName5> <ISBN5> <Author5> <Publisher5>" << endl;
+        return 1;
+    }
+
+    // Create an array of 5 Book objects using command-line arguments
     Book books[5] = {
-        Book("The Catcher in the Rye", "978-0-316-76948-0", "J.D. Salinger", "Little, Brown and Company"),
-        Book("1984", "978-0-452-28423-4", "George Orwell", "Secker & Warburg"),
-        Book("To Kill a Mockingbird", "978-0-06-112008-4", "Harper Lee", "J.B. Lippincott & Co."),
-        Book("The Great Gatsby", "978-0-7432-7356-5", "F. Scott Fitzgerald", "Scribner"),
-        Book("Moby-Dick", "978-0-14-243724-7", "Herman Melville", "Harper & Brothers")
+        Book(argv[1], argv[2], argv[3], argv[4]),
+        Book(argv[5], argv[6], argv[7], argv[8]),
+        Book(argv[9], argv[10], argv[11], argv[12]),
+        Book(argv[13], argv[14], argv[15], argv[16]),
+        Book(argv[17], argv[18], argv[19], argv[20])
     };
 
-    for (int i = 0; i < 5; ++i) {
-        cout << "Book " << i + 1 << " Information:\n";
-        cout << books[i].getBookInfo() << "\n\n";
+    // Display the information of each book
+    for (int i = 0; i < 5; i++) {
+        cout << "\nBook " << (i + 1) << " Information:\n";
+        cout << books[i].getBookInfo() << endl;
     }
 
     return 0;
